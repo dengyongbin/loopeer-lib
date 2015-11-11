93f7ffd2ea71f1aa3372a26a330c0f62943bcc99
dengyongbin <dengyongbin@loopeer.com>
2015-11-11 11:25:49 +0800

    add verify

--- src/Sms/LuoSiMaoSms.php
+++ src/Sms/LuoSiMaoSms.php
@@ -10,8 +10,14 @@
  */
 namespace Loopeer\Sms;
 
+/**
+ * 短信平台发送短信和语音类
+ * Class LuoSiMaoSms
+ * @package Loopeer\Sms
+ */
 class LuoSiMaoSms {
 
+    // 短信或语音key
     protected $apiKey;
 
     public function __construct($apiKey) {
@@ -20,29 +26,58 @@ class LuoSiMaoSms {
     public function setApiKey($apiKey) {
         $this->apiKey = $apiKey;
     }
+
     /**
+     * 发送短信
      * @param $phone
-     * @param $msg 验证码：%s，请于30分钟内输入使用 【绿葡科技】
-     * @param $captcha
+     * @param $message
      * @return mixed
      */
-    public function sendSms($phone, $msg, $captcha) {
+    public function sendSms($phone, $message) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "http://sms-api.luosimao.com/v1/send.json");
         curl_setopt($ch, CURLOPT_ENCODING ,"UTF-8");
+        $this->setOpt($ch);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+                'mobile' => $phone,
+                'message' => $message
+            )
+        );
+        $res = curl_exec($ch);
+        curl_close($ch);
+        return $res;
+    }
+
+    /**
+     * 发送语音
+     * @param $phone
+     * @param $code
+     * @return mixed
+     */
+    public function sendVerify($phone, $code) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://voice-api.luosimao.com/v1/verify.json");
+        $this->setOpt($ch);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+                'mobile' => $phone,
+                'code' => $code
+            )
+        );
+        $res = curl_exec( $ch );
+        curl_close( $ch );
+        return $res;
+    }
+
+    /**
+     * 设置curl参数
+     * @param $ch
+     */
+    private function setOpt($ch) {
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($ch, CURLOPT_USERPWD, $this->apiKey);
         curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-                'mobile' => $phone,
-                'message' => sprintf($msg, $captcha)
-            )
-        );
-        $res = curl_exec($ch);
-        curl_close($ch);
-        return $res;
     }
 }
---
+++ test/JPushTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (C) Loopeer, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ *
+ * User: DengYongBin
+ * Date: 15/6/5
+ * Time: 下午2:00
+ */
+use Loopeer\Push;
+
+class JPushTest extends PHPUnit_Framework_TestCase {
+
+    public function push() {
+        $jpush = new Loopeer\Push\JPush('dd1066407b044738b6479275', '6b135be0037a5c1e693c3dfa', false);
+        $jpush->pushNotification('0a0e477959a', 'test jpush msg', null, null);
+    }
+}
