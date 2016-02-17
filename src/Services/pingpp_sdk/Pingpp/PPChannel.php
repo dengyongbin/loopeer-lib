7437c5f67df5e185bfbf5a763d7764c1fa2af968
WangKaibo <wangkaibo@loopeer.com>
2016-02-17 11:50:05 +0800

    add pingpp sdk

---
+++ src/Services/pingpp_sdk/Pingpp/PPChannel.php
@@ -0,0 +1,12 @@
+<?php
+
+abstract class PPChannel
+{
+    /*
+     * Constants to represent third party payment service providers
+     * accroding to http://pingplusplus.github.io/pingpp-api/
+     */
+    const ALIPAY  = "alipay";
+    const WECHAT  = "wx";
+    const UPMP    = "upmp";
+}
